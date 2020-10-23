0d9253a076171e000e1a539a0864ef0cace2a1d8
Bogdan Zhila <zhilab1993@gmail.com>
2020-10-23 16:48:54 +0300

    mongo

---
+++ tasks/taskControllerMongo.js
@@ -0,0 +1,166 @@
+const fs = require('fs');
+const Task = require('./taskModel');
+
+/*function taskData(data) {
+	this.id = data._id;
+	this.title= data.title;
+}*/
+
+/**
+ * Повертає список тасків з файлу
+ */
+function fetchTask() {
+    try {
+       let tasksStrng = fs.readFileSync(patchParams);
+
+       console.log(`JSON read file`);
+
+       return JSON.parse(tasksStrng);
+        
+    } catch (error) {
+        console.log(`Error read file - ${error} `);
+        return [];
+    }
+};
+
+/**
+ * 
+ * Додає нове завдання
+ */
+async function addTask(titleData)
+{
+
+    const task = {
+        title : titleData
+    }
+
+    await saveTask(task)         //тут потрібно await ???
+        .then((taskData)=>{ 
+
+            console.log("Add");
+            logTask(taskData);
+        
+            return taskData;
+         })
+        .catch((reject) => {
+            console.log("rejectSave:" + reject);
+
+            return reject;
+        });
+};
+/** 
+ * Зберігає в Mongo
+*/
+
+//async function saveTask (tasks) {
+async function saveTask (taskData) {
+    const task = new Task(taskData);
+ 
+    try {
+        const result = await task.save();
+        console.log(result);
+
+        return result;
+    }catch (err) {
+        console.log(err.message)
+    }
+
+};
+
+/**
+ *  Запит який повертає масив значень по номеру сторінки і кільсоксті записів
+ */
+function queryTaskPage(page, limit) {
+
+    const tasks = fetchTask();
+
+    const sampleEnd = page * limit;
+    const sampleStart = sampleEnd - limit;
+
+    const simple = tasks.filter( (t)=>t.id >=sampleStart && t.id < sampleEnd);
+   
+    return simple;
+} 
+
+/**
+ * Повертає таск по id
+ */
+function getTask(idTask) {
+
+    const tasks = fetchTask();
+
+    const task = tasks.find(t=>t.id === idTask);
+
+    if (task === undefined)
+        return undefined;
+
+    console.log("Get Task");
+    logTask(task);
+
+    return task;
+}
+
+/**
+ * Редагує таск по id
+ */
+function editTask(idTask, title) {
+
+    const tasks = fetchTask();
+    let task = tasks.find(t=>t.id === idTask);
+
+    if(!task || task == null)
+    {
+        return null;
+    }
+
+    task.title = title;
+    saveTask(tasks);
+
+    console.log("Edit Task");
+    logTask(task);
+
+
+    return task;
+}
+
+/**
+ * Видаляє тас по id
+ */
+function deleteTask(idTask) {
+
+    const tasks = fetchTask();
+
+    const idDelete = tasks.findIndex(t => t.id === idTask);
+
+    if (idDelete > -1)
+    {
+        const task = tasks.splice(idDelete, 1);
+        saveTask(tasks);
+
+        console.log("Delete Task");
+        logTask(task);  
+
+        return task;
+    }else{
+        return null;
+    }
+}
+/**
+ * Виведнння таску в консоль
+ */
+function logTask(task) {
+    console.log('----------');
+    console.log(`id: ${task.id}`);
+    console.log(`title: ${task.title}`);
+    
+}
+
+module.exports = {
+    addTask,
+    deleteTask,
+    fetchTask,
+    queryTaskPage,
+    getTask,
+    editTask,
+    saveTask,
+};
---
+++ tasks/taskMongoApiRouter.js
@@ -0,0 +1,131 @@
+const express = require("express");
+const Joi = require('joi');
+const taskModul = require('../tasks/taskControllerMongo'); 
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+    const task = {
+        title : "zavdania"
+    }
+     taskModul.saveTask(task)
+        .then((taskData)=>{ console.log(`Task data = ${taskData}`); res.send(taskData);})
+        .catch((reject) => {
+            console.log("rejectSave:" + reject);
+        });
+  
+ 
+});
+
+router.get('/page', (req, res) => {
+
+    const { page = 1, limit = 10 } = req.query;
+
+    if (Number.isNaN(page) || Number.isNaN(limit) || page <= 0 || limit <= 0)
+    {
+        return res.status(400).send("Error parameters");
+    }
+
+    const simple = taskModul.queryTaskPage(page, limit)
+    res.send(simple);
+});
+
+router.get('/:id',(req, res) => {
+    const idTask = parseInt(req.params.id);
+
+    if (Number.isNaN(idTask))
+    {
+        return res.status(400).send("Error parameters");
+    }
+
+    let task = taskModul.getTask(idTask);
+
+    if(!task || task === null || task === undefined)
+    {
+        //res.status(404).send("The task not found");
+        res.status(404);
+        return res.send(`The task  id=${idTask} not found`);
+    }
+
+    res.send(task);
+});
+
+router.post('/', (req, res) => {
+  
+    const schema = Joi.object({
+        title: Joi.string().min(3).max(300).required()
+    });
+
+    const valid = schema.validate(req.body);
+
+    if(valid.error)
+    {
+        res.status(400).send(valid.error);
+        return valid.error;
+    }
+
+    console.log(`reg body ${req.body.title}`);
+    taskModul.addTask(req.body.title).then((taskData)=>{
+
+        console.log("PUT DATA:" + taskData);
+        res.send(taskData);
+        
+    })
+    .catch((reject) => {
+            console.log("rejectSave:" + reject);
+        });
+
+   
+});
+
+router.put('/:id', (req, res) => {
+    idTask = parseInt(req.params.id);
+
+    const schema = Joi.object({
+        title: Joi.string().min(3).max(300).required()
+    });
+
+    const valid = schema.validate(req.body);
+
+    if(valid.error)
+    {
+        res.status(400).send(valid.error);
+        return valid.error;
+    }
+
+    const task = taskModul.editTask(idTask, req.body.title);
+
+    if(!task || task == null)
+    {
+        res.status(404);
+        return res.send(`The task  id=${idTask} not found`);
+    }
+
+    console.log(`edit task id ${idTask}`);
+
+    res.send(task);
+});
+
+router.delete('/:id', (req, res) => {
+   
+    const idTask = parseInt(req.params.id);
+
+    if (Number.isNaN(idTask))
+    {
+        return res.status(400).send("Error parameters");
+    }
+
+    const task = taskModul.deleteTask(idTask);
+    
+    if (task != null)
+    {
+        res.send(task);
+    }else{
+        res.status(404).send("No found element");
+    }  
+    
+});
+
+module.exports = router;
+
+
