d165b1653e4d0bf046fde3667eb4932591e294aa
Bogdan Zhila <zhilab1993@gmail.com>
2020-10-29 19:46:47 +0200

    edit

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const taskController = require('./tasks/mongo_metod/taskController');
 
 const mongoose = require('mongoose');
 
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://botak:asdfghjkl123@cluster0.wtkub.mongodb.net/todos?retryWrites=true&w=majority'
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(
+    MONGODB_URL, 
+    { useNewUrlParser: true, useUnifiedTopology: true }
+    )
     .then(() => console.log(`Now connected to MongoDB! url = ${MONGODB_URL}`))
     .catch(err => console.error('Something went wrong', err));
 
-const app = express();
+
 app.use(express.json());
 
 app.use('/', indexRouter);
@@ -19,6 +27,6 @@ app.use('/api/tasks', taskAPIRouter);
 app.use('/api/mongo', taskController);
 
 
-const PORT = process.env.PORT||3000;
 
-app.listen(PORT, ()=>{console.log(`Servers start PORT = ${PORT}`)});
+
+app.listen(PORT, () => { console.log(`Servers start PORT = ${PORT}`) });
