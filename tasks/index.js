8e29f34d1b4704ced8589484b7d40e3ec8bc767a
Bogdan Zhila <zhilab1993@gmail.com>
2020-10-29 18:06:05 +0200

    edit

--- tasks/index.js
+++ tasks/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get('/',function(req, res, next){
-    //res.send('INDEX');
-    res.redirect(301,'/api/tasks');
+router.get('/', function(req, res, next){
+    res.redirect(301, '/api/tasks');
 });
 
 module.exports = router;
