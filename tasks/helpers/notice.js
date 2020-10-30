99cfef8d920aa729b882abc83566e36de0933f52
Bogdan Zhila <zhilab1993@gmail.com>
2020-10-30 14:58:48 +0200

    add notice

---
+++ tasks/helpers/notice.js
@@ -0,0 +1,18 @@
+
+const errNote = {
+      "e400" :"Error parameters",
+      "e404" : "Not found response",
+      "e500" : "Internal Server Error"
+    }
+    
+const successNote = {
+        "pagination" : "pagination",
+        "getId" : "get:id",
+        "post" : "post",
+        "put" : "put",
+        "delete" : "delete"
+      }
+  
+
+  
+  
