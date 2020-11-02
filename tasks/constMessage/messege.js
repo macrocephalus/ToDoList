f716f85cefde215bd04bea6d8ff162c28085b9b7
Bogdan Zhila <zhilab1993@gmail.com>
2020-11-02 17:28:41 +0200

    still code

--- tasks/constMessage/messege.js
+++ tasks/constMessage/messege.js
@@ -1,16 +1,16 @@
 const messegRes = {
   err: {
-      "e400": "Error parameters",
-      "e404": "Not found response",
-      "e500": "Internal Server Error"
-    },
-    success: {
-      "pagination": "pagination",
-      "getId": "get:id",
-      "post": "post",
-      "put": "put",
-      "delete": "delete"
-    }
-  }
-  
+    e400: 'Error parameters',
+    e404: 'Not found response',
+    e500: 'Internal Server Error',
+  },
+  success: {
+    pagination: 'pagination',
+    getId: 'get:id',
+    post: 'post',
+    put: 'put',
+    delete: 'delete',
+  },
+};
+
 module.exports = messegRes;
